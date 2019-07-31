6abfc80172c88b9113b27885b6fd77ea6f79865a
andrew minucos <minucos@gmail.com>
2019-07-30 17:09:04 -0700

    remove active_storage routes

--- demos/BlueBird/config/application.rb
+++ demos/BlueBird/config/application.rb
@@ -1,6 +1,25 @@
 require_relative 'boot'
 
-require 'rails/all'
+# require 'rails/all'
+
+require "rails"
+
+# Includes each railties manually, excluding `active_storage/engine`
+%w(
+  active_record/railtie
+  action_controller/railtie
+  action_view/railtie
+  action_mailer/railtie
+  active_job/railtie
+  action_cable/engine
+  rails/test_unit/railtie
+  sprockets/railtie
+).each do |railtie|
+  begin
+    require railtie
+  rescue LoadError
+  end
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
--- demos/BlueBird/config/environments/development.rb
+++ demos/BlueBird/config/environments/development.rb
@@ -28,7 +28,9 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+
+  # COMMENT BACK IN TO USE ACTIVE_STORAGE
+  # config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
