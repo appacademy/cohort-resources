8a43dcb51a199a6f65e02573a63cf38cdfad4de5
Mike Madsen <mikewmadsen@gmail.com>
2019-07-31 11:42:32 -0700

    Complete W6D3 routes and controllers demo

---
+++ demos/BlueBird/app/controllers/users_controller.rb
@@ -0,0 +1,43 @@
+class UsersController < ApplicationController
+
+  def index
+    users = User.all
+
+    render json: users 
+  end
+
+  def show
+    user = User.find(params[:id])
+
+    render json: user
+  end
+
+  def create
+    debugger
+    user = User.new(user_params)
+    
+    if user.save
+      render json: user 
+    else
+      render json: user.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    user = User.find(params[:id])
+
+    if user.update(user_params)
+      render json: user 
+    else
+      render json: user.errors.full_messages, status: 422
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :age, :political_affiliation)
+  end
+
+  
+end
--- demos/BlueBird/config/initializers/content_security_policy.rb
+++ demos/BlueBird/config/initializers/content_security_policy.rb
@@ -23,3 +23,5 @@
 # For further information see the following documentation:
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
 # Rails.application.config.content_security_policy_report_only = true
+
+Rails.application.config.action_controller.default_protect_from_forgery = false
