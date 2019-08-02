cc23cc7c698f5b89761b46754c843e2e1af4ea3c
Alissa <alissacrane123@gmail.com>
2019-08-02 12:00:10 -0700

    rails views demo

--- demos/BlueBird/app/controllers/users_controller.rb
+++ demos/BlueBird/app/controllers/users_controller.rb
@@ -1,42 +1,51 @@
 class UsersController < ApplicationController
 
   def index
-    users = User.all
+    @users = User.all
 
-    render json: users 
+    render :index
   end
 
   def show
-    user = User.find(params[:id])
+    @user = User.find(params[:id])
 
-    render json: user
+    render :show
+  end
+
+  def new 
+    @user = User.new 
+    render :new 
   end
 
   def create
-    debugger
-    user = User.new(user_params)
+    @user = User.new(user_params)
     
-    if user.save
-      render json: user 
+    if @user.save
+      redirect_to user_url(@user)
     else
-      render json: user.errors.full_messages, status: 422
+      render json: @user.errors.full_messages, status: 422
     end
   end
 
-  def update
-    user = User.find(params[:id])
+  def edit 
+    @user = User.find(params[:id])
+    render :edit 
+  end
 
-    if user.update(user_params)
-      render json: user 
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to user_url(@user) 
     else
-      render json: user.errors.full_messages, status: 422
+      render json: @user.errors.full_messages, status: 422
     end
   end
 
   def destroy
-    user = User.find(params[:id])
-    user.destroy
-    render json: user
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_url 
   end
 
   private
