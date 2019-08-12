1a1204c657b434874e00ba9bfb26f7fde9048752
Carlos Garcia <carlosaicrag@gmail.com>
2019-08-12 09:21:02 -0700

    adds Bluebird to 06-24-2019 branch with updated nested route changes

--- demos/BlueBird/app/controllers/sessions_controller.rb
+++ demos/BlueBird/app/controllers/sessions_controller.rb
@@ -1,29 +1,26 @@
 class SessionsController < ApplicationController
-    def new
-        @user = User.new
-        render :new
-    end
+  before_action :require_logged_out, only: [:new, :create]
 
-    def create
-        @user = User.find_by_credentials(
-            params[:user][:username], #argument
-            params[:user][:password] #argument
-        )
-        
-        if @user 
-            login!(@user)
-            redirect_to user_url(@user)
-        else
-            @user = User.new
-            flash.now[:errors] = ["Invalid Credentials"]
-            render :new
-        end
+  def new
+  end
 
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username], 
+      params[:user][:password]
+    )
 
-    end
+    if @user
+      login!(@user)
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = ["you messed up"]
+      render :new
+    end 
+  end 
 
-    def destroy 
-        log_out!
-        redirect_to new_session_url
-    end
+  def destroy
+    logout!
+    redirect_to new_session_url
+  end 
 end
