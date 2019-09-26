f141150e589e9e2605fdff9e831d3d60a58f8075
Carlos Garcia <carlosaicrag@gmail.com>
2019-09-26 11:10:28 -0700

    deletes bluebird from branch

--- demos/BlueBird/db/migrate/20190807054306_add_user_auth.rb
+++
@@ -1,7 +0,0 @@
-class AddUserAuth < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :password_digest, :string, null: false
-    add_column :users, :session_token, :string, null: false
-    add_index :users, :session_token, unique: true
-  end
-end
