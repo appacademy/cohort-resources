a99be5ee52583a88651014331cf25fcc959bbae6
Carlos Garcia <carlosaicrag@gmail.com>
2019-10-13 20:32:38 -0700

    remove bluebird from demos folder

--- demos/bluebird-aug-2019-capybara-demo/db/migrate/20190920171633_add_email_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddEmailToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :email, :string, null: false 
-    add_index :users, :email, unique: true 
-  end
-end
-
-# need to add index for unique: true 
