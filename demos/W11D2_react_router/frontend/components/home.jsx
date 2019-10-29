87e28bdd7a7d3195992b2869d93baef7ec4641b0
Carlos Garcia <carlosaicrag@gmail.com>
2019-10-29 13:57:58 -0700

    Add react router demo

---
+++ demos/W11D2_react_router/frontend/components/home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = (props) => {
+
+  debugger
+  return(
+  <section className="home">
+      <h1><font color="white">This is The Super Cool Home Page!</font></h1>
+      <h1 className="sub-header">[ Home Component ]</h1>
+  </section>
+  );
+};
+
+export default Home;
