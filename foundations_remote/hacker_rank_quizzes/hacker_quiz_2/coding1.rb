dbe6b04d79c479af2965f7b401d06adda75f42b7
Carlos Garcia <carlosaicrag@gmail.com>
2019-12-13 16:30:21 -0800

    rename folders and remove unneccesary folder

--- foundations_remote/hacker_rank_quizzes/hacker_quiz_2/coding1.rb
+++
@@ -1,8 +0,0 @@
-#write a method, blocker, that accepts two values and a block. the method 
-#should return the result of the block when it is passed both values.
-
-# Examples
-p blocker(4, 5) { |a, b| a * b } # => 20
-p blocker(3, 2) { |a, b| a * b } # => 6
-p blocker("Hello", "World") { |a, b| a.upcase + " " + b.downcase + "!" } # => "HELLO world!"
-p blocker("cellar", "door") { |a, b| a.upcase + " " + b.downcase + "!" } # => "CELLAR door!"
