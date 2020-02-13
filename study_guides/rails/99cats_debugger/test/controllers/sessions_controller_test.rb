229e9946d2284e5b5be684c37ebbe3bb8d04575b
Mike Madsen <mikewmadsen@gmail.com>
2020-02-13 15:37:18 -0800

    Delete extraneous folders

--- study_guides/rails/99cats_debugger/test/controllers/sessions_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get sessions_new_url
-    assert_response :success
-  end
-
-end
