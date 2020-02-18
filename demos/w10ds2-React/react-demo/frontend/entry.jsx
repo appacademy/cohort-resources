863b38f91a7bc3c80dc6b92fcc6425ba530e5d37
joe bumbaca <joebumbaca@gmail.com>
2020-02-18 10:21:51 -0800

    NPM setup

---
+++ demos/w10ds2-React/react-demo/frontend/entry.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", () => {
+  // debugger;
+  const root = document.getElementById('root');
+  ReactDom.render(<h1>React is working bruh!</h1>, root)
+});
