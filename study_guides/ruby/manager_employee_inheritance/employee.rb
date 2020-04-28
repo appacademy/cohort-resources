1739bec04f341b84107139817c329cc16cd4ae30
Carlos Garcia <carlosaicrag@gmail.com>
2020-04-28 14:58:18 -0700

    add class_inheritance solution

--- study_guides/ruby/manager_employee_inheritance/employee.rb
+++ study_guides/ruby/manager_employee_inheritance/employee.rb
@@ -1,50 +1,25 @@
-require "byebug"
-
 class Employee
-    attr_reader :name, :salary, :title, :boss
-    def initialize(name,salary,title,boss)
-        @name = name 
-        @salary = salary
-        @title = title
-        @boss = boss
+  attr_reader :name, :salary, :title
+  attr_accessor :boss
 
-        if @boss
-            @boss.subordinates.push(self)
-        end
+  def initialize(name, salary, title, boss = nil)
+    @name, @salary, @title = name, salary, title
+    self.boss = boss
 
-    end
+    @boss = boss
+    # if @boss
+    #     @boss.employees.push(self)
+    # end
+  end
 
-    def bonus(multiplier)
-        if(self.instance_of?(Employee))
-            bonus = self.salary*multiplier
-            return bonus
-        else
-            bonus = []
+  def boss=(boss)
+    @boss = boss
+    boss.add_employee(self) unless boss.nil?
 
-            self.subordinates.each do |employee|
-                if employee.instance_of?(Manager)
-                    bonus << employee.salary
-                    bonus << employee.bonus(1)
-                else
-                    bonus << employee.bonus(1)
-                end
-            end
+    boss
+  end
 
-            return bonus.sum * multiplier
-        end
-    end
-
-    def inspect 
-        "#{name} #{salary}"
-    end
-
-    def call_say_yo
-        say_yo
-    end
-
-    private
-    def say_yo
-        puts "yo"
-    end
+  def bonus(multiplier)
+    self.salary * multiplier
+  end
 end
-
--- study_guides/ruby/manager_employee_inheritance/manager.rb
+++ study_guides/ruby/manager_employee_inheritance/manager.rb
@@ -1,29 +1,36 @@
-
-require_relative "employee"
+require_relative "./employee.rb"
 
 class Manager < Employee
+  attr_reader :employees
 
-    attr_reader :subordinates
+  def initialize(name, salary, title, boss = nil)
+    # super(name, salary, title, boss)
+    super
+    @employees = []
+  end
 
-    def initialize(name,salary,title,boss)
-        super(name,salary,title,boss) 
-        @subordinates = []
+  def add_employee(subordinate)
+    employees << subordinate
+
+    subordinate
+  end
+
+  def bonus(multiplier)
+    self.total_subsalary * multiplier
+  end
+
+  protected
+  
+  def total_subsalary
+    total_subsalary = 0
+    self.employees.each do |employee|
+      if employee.is_a?(Manager)
+        total_subsalary += employee.salary + employee.total_subsalary
+      else
+        total_subsalary += employee.salary
+      end
     end
 
-
-
-    
+    total_subsalary
+  end
 end
-
-ned = Manager.new("ned",1000000,"founder",nil)
-darren = Manager.new("darren",78000,"TA Manager",ned)
-shawna = Employee.new("shawna",12000,"TA",darren)
-david = Employee.new("david",10000,"TA",darren)
-
-# p darren.bonus(4)
-# p ned.bonus(5)
-# p david.bonus(3)
-
-darren.call_say_yo
-shawna.call_say_yo
-
