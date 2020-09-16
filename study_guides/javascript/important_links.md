2bb1cffb515bb21bead8f7f787b6cfc2aeb1c412
joe bumbaca <joebumbaca@gmail.com>
2020-09-16 14:02:27 -0700

    Set up branch for new SF cohort

--- study_guides/javascript/important_links.md
+++
@@ -1,4 +0,0 @@
-* list of events 
-    * https://developer.mozilla.org/en-US/docs/Web/Events
-* all webAPIs
-    * https://developer.mozilla.org/en-US/docs/Web/API
--- study_guides/react_redux/pokedex_debugger/frontend/reducers/root_reducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import entities from './entities_reducer';
-import ui from './ui_reducer';
-
-export default combineReducers({
-  entities,
-  ui,
-});
