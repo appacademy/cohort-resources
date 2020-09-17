84e83377e5f4d90bd738d0e8fa01e3d1fb5d1de5
Andrew Minucos <minucos@gmail.com>
2020-09-17 09:22:38 -0400

    creation of 08-31-2020 branch

--- assessment_tips/rails_2/rails_2_buggy_version/spec/models/goal_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe Goal, type: :model do
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:details) }
-  it { should allow_value(true, false).for(:status) }
-  it { should belong_to(:user) }
-end
--- study_guides/react_redux/pokedex_debugger/frontend/components/pokemon/loading_icon.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const LoadingIcon = () => (
-  <div id="loading-pokeball-container">
-    <div id="loading-pokeball"></div>
-  </div>
-);
-
-export default LoadingIcon;
