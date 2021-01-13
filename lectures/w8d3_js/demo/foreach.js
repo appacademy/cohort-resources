1b42223812ebbfed9e69e524349630a94fdef975
Carlos Garcia <carlosaicrag@gmail.com>
2021-01-13 12:20:09 -0800

    add for each demo

--- lectures/w8d3_js/demo/foreach.js
+++ lectures/w8d3_js/demo/foreach.js
@@ -3,6 +3,12 @@
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
 
+  arr = [1,2,3]
+
+  arr.forEach((el) => {
+    console.log(el)
+  })
+
   
   
 
