2302cce39e81cc981afb02c0d4f6b6e484fd251d
Carlos Garcia <carlosaicrag@gmail.com>
2021-01-13 12:10:13 -0800

    add w8d3 javascript lecture

---
+++ lectures/w8d3_js/demo/objects.js
@@ -0,0 +1,17 @@
+let obj = {}
+obj.name = "banana"
+obj.arr = [1,2,3,4]
+obj.inner_obj = {name: "carlos"}
+
+obj.func = function() {
+  debugger
+  console.log("I print out things")
+}
+
+func = obj.func
+func()
+
+
+
+
+
