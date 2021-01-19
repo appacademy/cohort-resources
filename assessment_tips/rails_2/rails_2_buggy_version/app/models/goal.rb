be5d471a7911458de5cde192be7b1ff6c631e901
Michelle Kim <x1michelleskim@gmail.com>
2021-01-19 15:23:14 -0800

    Start new branch for new cohort

--- assessment_tips/rails_2/rails_2_buggy_version/app/models/goal.rb
+++
@@ -1,8 +0,0 @@
-class Goal < ApplicationRecord
-    validates :name, :details, presence: true
-    validates :status, inclusion: { in: [:true, :false] }
-
-    belongs_to :user,
-    foreign_key: :owner_id,
-    class_name: "User"
-end
