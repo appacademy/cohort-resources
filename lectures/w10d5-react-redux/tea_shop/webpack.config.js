d86a17d33bb060c8e37baeb94f3577dae4eff361
iProgYou <zack.musicteacher@gmail.com>
2021-01-29 12:09:31 -0800

    add react redux slides

---
+++ lectures/w10d5-react-redux/tea_shop/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './frontend/tea_shop.jsx',
+  output: {
+    path: path.resolve(__dirname), 
+    filename: './bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/], 
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/env','@babel/react'] 
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '*','.jsx'] 
+  }
+}
