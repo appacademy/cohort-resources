4a3f9b039ac5505df7c7fefc9f798fcfb49ffa47
iProgYou <zack.musicteacher@gmail.com>
2021-02-05 15:32:11 -0800

    add files

---
+++ lectures/ruby_1_prep/spec/assessment01_spec.rb
@@ -0,0 +1,141 @@
+require 'rspec'
+require 'assessment01'
+
+describe 'Array#my_inject' do
+
+  it 'calls the block passed to it' do
+    expect do |block|
+      ["test array"].my_inject(:dummy, &block)
+    end.to yield_control.once
+  end
+
+  it 'makes the first element the accumulator if no default is given' do
+    expect do |block|
+      ["el1", "el2", "el3"].my_inject(&block)
+    end.to yield_successive_args(["el1", "el2"], [nil, "el3"])
+  end
+
+  it 'yields the accumulator and each element to the block' do
+    expect do |block|
+      [1, 2, 3].my_inject(100, &block)
+    end.to yield_successive_args([100, 1], [nil, 2], [nil, 3])
+  end
+
+  it 'does NOT call the built in Array#inject or Array#reduce method' do
+    original_array = ["original array"]
+    expect(original_array).not_to receive(:inject)
+    expect(original_array).not_to receive(:reduce)
+    original_array.my_inject {}
+  end
+
+  it 'with accumulator, it correctly injects and returns answer' do
+    expect([1, 2, 3].my_inject(1) { |acc, x| acc + x }).to eq(7)
+    expect([3, 3].my_inject(3) { |acc, x| acc * x }).to eq(27)
+  end
+
+  it 'without accumulator, it correctly injects and returns answer' do
+    expect([1, 2, 3].my_inject { |acc, x| acc + x }).to eq(6)
+    expect([3, 3].my_inject { |acc, x| acc * x }).to eq(9)
+  end
+end
+
+describe "#primes" do
+
+  it "returns first five primes in order" do
+    expect(primes(5)).to eq([2, 3, 5, 7, 11])
+  end
+
+  it "returns an empty array when asked for zero primes" do
+    expect(primes(0)).to eq([])
+  end
+end
+
+describe "#factorials_rec" do
+  it "returns first factorial number" do
+    expect(factorials_rec(1)).to eq([1])
+  end
+
+  it "returns first two factorial numbers" do
+    expect(factorials_rec(2)).to eq([1, 1]) # = [0!, 1!]
+  end
+
+  it "returns many factorials numbers" do
+    expect(factorials_rec(6)).to eq([1, 1, 2, 6, 24, 120])
+    # == [0!, 1!, 2!, 3!, 4!, 5!]
+  end
+
+  it "calls itself recursively" do
+    # this should enforce you calling your method recursively.
+
+    expect(self).to receive(:factorials_rec).at_least(:twice).and_call_original
+    factorials_rec(6)
+  end
+end
+
+describe "#dups" do
+  it "solves a simple example" do
+    expect([1, 3, 0, 1].dups).to eq({ 1 => [0, 3] })
+  end
+
+  it "finds two dups" do
+    expect([1, 3, 0, 3, 1].dups).to eq({ 1 => [0, 4], 3 => [1, 3] })
+  end
+
+  it "finds multi-dups" do
+    expect([1, 3, 4, 3, 0, 3].dups).to eq({ 3 => [1, 3, 5] })
+  end
+
+  it "returns {} when no dups found" do
+    expect([1, 3, 4, 5].dups).to eq({})
+  end
+end
+
+describe "#symmetric_substrings" do
+
+  it "handles a simple example" do
+    expect("aba".symmetric_substrings).to match_array(["aba"])
+  end
+
+  it "handles two substrings" do
+    expect("aba1cdc".symmetric_substrings).to match_array(["aba", "cdc"])
+  end
+
+  it "handles nested substrings" do
+    expect("xabax".symmetric_substrings).to match_array(["aba", "xabax"])
+  end
+end
+
+describe "#merge_sort" do
+  let(:array) { [1, 2, 3, 4, 5].shuffle }
+
+  it "works with an empty array" do
+    expect([].merge_sort).to eq([])
+  end
+
+  it "works with an array of one item" do
+    expect([1].merge_sort).to eq([1])
+  end
+
+  it "sorts numbers" do
+    expect(array.merge_sort).to eq(array.sort)
+  end
+
+  it "will use block if given" do
+    reversed = array.merge_sort do |num1, num2|
+      # reverse order
+      num2 <=> num1
+    end
+    expect(reversed).to eq([5, 4, 3, 2, 1])
+  end
+
+  it "does not modify original" do
+    duped_array = array.dup
+    duped_array.merge_sort
+    expect(duped_array).to eq(array)
+  end
+
+  it "calls the merge helper method" do
+    expect(Array).to receive(:merge).at_least(:once).and_call_original
+    array.merge_sort
+  end
+end
---
+++ lectures/w10d4_redux/tea_shop/frontend/components/tea_index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import TeaForm from './tea_form';
+
+const TeaIndex = (props) => {
+    debugger
+    return(
+        <div>
+            <h1>All the teas</h1>
+            <ul>
+                {props.teas.map((tea) => ( // implicit return, no return statement needed
+                    <li key={tea.id}>flavor: {tea.flavor}</li>
+                ))}
+            </ul>
+            <h3>Green Teas</h3>
+            <ul>
+                {props.greenTeas.map(tea => { // explicit return, return statement necessary 
+                    return <li key={tea.id}>flavor: {tea.flavor}</li>
+                })}
+            </ul>
+            {/* <TeaForm /> // TeaForm will not have receiveTea in this case. Must be passed as a prop */}
+            <TeaForm receiveTea={props.receiveTea}/>
+        </div>
+    );
+}
+
+export default TeaIndex;
