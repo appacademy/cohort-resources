39b7f2ad77c79d20a53b34fd2c2c2ab70bf811dc
Samuel Walker <sewalker191@gmail.com>
2021-03-23 10:08:52 -0700

    Add first demo of w10d2-react

---
+++ lectures/w10d2-react/demo/puppy_survey/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!***************************!*\
+  !*** ./frontend/entry.js ***!
+  \***************************/
+console.log("Webpack is working!");
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
