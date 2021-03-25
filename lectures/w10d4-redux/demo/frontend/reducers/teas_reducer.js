4b33f8c68e1176d46233e425d876e35248d5e32c
Vanessa Chen <vanchen07@gmail.com>
2021-03-25 11:32:18 -0700

    Add w10d4-redux slides and demo

---
+++ lectures/w10d4-redux/demo/frontend/reducers/teas_reducer.js
@@ -0,0 +1,27 @@
+import { RECEIVE_TEA } from '../actions/tea_actions';
+
+const teasReducer = (state = {}, action) => {
+    Object.freeze(state);  
+    const nextState = Object.assign({}, state);
+
+    // {
+    // type: 'RECEIVE_TEA',
+    // tea: {
+    //     flavor: 'green',
+    //     amount: 2,
+    //     id: 1
+    // }
+    // }
+
+    //{1: {flavor: ', amount: '', id: 1}}
+
+    switch (action.type) {
+        case RECEIVE_TEA:
+            nextState[action.tea.id] = action.tea;
+            return nextState;
+        default: 
+            return state;
+    }
+}
+
+export default teasReducer; 
