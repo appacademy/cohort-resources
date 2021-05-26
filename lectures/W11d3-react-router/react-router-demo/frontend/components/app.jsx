ced1d2856b0cc3b1f118835d43e56c42686980b3
iProgYou <zack.musicteacher@gmail.com>
2021-05-26 13:46:39 -0700

    adding slides

---
+++ lectures/W11d3-react-router/react-router-demo/frontend/components/app.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import TodoIndexContainer from './todo_index/todo_index_container';
+import TodoFormContainer from './todo_index/todo_form_container';
+import TodoShowContainer from './todo_index/todo_show_container';
+import Error from './error';
+import Header from './header';
+import Home from './home';
+
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+
+const App = () => (
+  <div className="app">
+    <Header />
+    <Switch>
+      <Route path="/todos/new" component={TodoFormContainer} />
+      <Route path="/todos/:id" component={TodoShowContainer} />
+      <Route path="/todos" component={TodoIndexContainer} />
+      <Route exact path="/" component={Home} />
+      <Route path="/error" component={Error}/>
+      <Redirect to="/error" />
+    </Switch>
+
+    {/* <Home />
+    <TodoFormContainer />
+    <TodoIndexContainer /> */}
+  </div>
+);
+
+export default App;
