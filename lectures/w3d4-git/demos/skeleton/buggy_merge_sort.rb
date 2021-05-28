f3493d6d83a2b02fbd2f6671691207bb08158367
Mike Madsen <mikewmadsen@gmail.com>
2021-05-28 12:11:23 -0700

    Add w3d5 data structures

--- lectures/w3d4-git/demos/skeleton/buggy_merge_sort.rb
+++ lectures/w3d4-git/demos/skeleton/buggy_merge_sort.rb
@@ -2,25 +2,27 @@ require "byebug"
 
 class Array
   def merge_sort(&prc) # Procifies the block
-    return self if arr.count <= 1
+    return self if self.count <= 1 # arr should be self
   
     middle = self.length / 2
     left = self[0...middle]
     right = self[middle..-1]
   
-    sorted_left = merge_sort(left)
-    sorted_right = merge_sort(right)
-    merge(sorted_left, sorted_right, prc)
+    sorted_left = left.merge_sort(&prc) # mergesort is monkeypatched
+    sorted_right = right.merge_sort(&prc)
+    Array.merge(sorted_left, sorted_right, prc)
   end
   
   private
-  def self.merge(left, right, prc)
+  def self.merge(left, right, prc) # class method
+    prc ||= Proc.new { |a,b| a <=> b } # default proc, spaceship compares a and b
+
     result = []
     until left.empty? || right.empty?
       if prc.call(left[0], right[0]) <= 0
-        result += left.shift
+        result << left.shift # += should be <<
       else
-        result += right.shift
+        result << right.shift
       end
     end
   
@@ -35,5 +37,5 @@ array = [5, 4, 300, 25, 21, 26, 24]
 p array.merge_sort
 
 # # passing in a block 
-# p array.merge_sort { |a, b| b <=> a }
+p array.merge_sort { |a, b| b <=> a }
 
