ba0b412b6b53c2df5acc17a6f681eb2b1b130ed8
Mike Madsen <mikewmadsen@gmail.com>
2021-06-29 14:54:11 -0700

    Add w8d2 js intro

---
+++ lectures/w8d2-js-intro/demo/variables.js
@@ -0,0 +1,139 @@
+/* const */
+
+// 1. can be declared and assigned ONLY once
+//      aka it must be initialized at time of declaration
+// 2. it is block scoped (scope?)
+
+// Scope: "The scope of a variable is the region of a program where it can be accessed."
+
+function scopeExample() {
+  // SCOPE A; variables: x
+  const x = "x";
+
+  // the code block is denoted by { and }
+  // this is what the "block" in "block scoped" refers to
+  if (true) {
+    // SCOPE B; variables: x, y
+    const y = "y";
+
+    // You can also make a code block by just inserting { and }
+    {
+      // SCOPE C; variables: x, y, and z
+      debugger
+      const z = "z";
+    }
+  }
+}
+
+// In JavaScript, "each variable is accessible in it's direct scope and all scopes nested within that scope"
+
+
+/* const */
+
+// 1. can be declared and assigned ONLY once
+//      (reasignment prohibited, mutations allowed)
+//      (it must be initialized at time of declaration)
+// 2. it is block scoped (scope?)
+// 3. use const when possible
+
+// What will happen here?
+function howToConst() {
+  const username = 'Harold';
+
+  if (true) {
+    const username = 'Url';
+
+    console.log(username);
+  }
+
+  console.log(username);
+}
+
+
+
+// What about here?
+function howToConst2() {
+  const username = 'Harold';
+
+  if (true) {
+    console.log(username);
+  }
+
+  console.log(username);
+}
+
+
+// What about here?
+function howToConst3() {
+  if (true) {
+    const username = 'Harold';
+    console.log(username);
+  }
+
+  console.log(username);
+}
+
+
+
+/* let */
+// 1. Can be declared only once, but reassignment is allowed
+// 2. Block scoped
+
+// What will happen here?
+function howToLet() {
+  let secret = "I'm secretly a cat.";
+
+  if (true) {
+    secret = "I'm secretly a banana";
+    console.log(secret);
+  }
+
+  console.log(secret);
+}
+
+
+// What about here?
+function howToLet2() {
+  let secret = "I'm secretly a cat.";
+
+  if (true) {
+    // secret = "I'm secretly a banana";
+    let secret = "Nope, definitely a cat.";
+    console.log(secret);
+
+  }
+
+  console.log(secret);
+}
+
+
+/* var */
+// 1. Function-scoped
+// 2. AVOID USING THIS, use let and const
+
+// What happens here?
+function howToVar() {
+  var username = 'Harold';
+
+  if (true) {
+    var username = 'Url';
+
+    console.log(username);
+  }
+
+  console.log(username);
+}
+
+
+// What about here?
+function howToVar2() {
+
+  if (true) {
+    var username = 'Url';
+
+    console.log(username);
+  }
+
+  console.log(username);
+}
+
