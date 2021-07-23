1b5f498ed448d5e3bb0b5aa4fb12802124c815f4
Jordan Tom <jordan.tom88@gmail.com>
2021-07-23 14:21:04 -0700

    Add jbuilder demo to w11d5 lecture

---
+++ lectures/w11d5-jbuilder/tea_time_no_jbuilder/test/models/user_test.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  username   :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
