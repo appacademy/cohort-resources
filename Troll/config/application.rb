ca318a41b111ae49ac86bee0b7ee11f0c5c7abe5
paulobocanegra <paulobocanegra@gmail.com>
2021-10-13 10:08:19 -0700

    Capybara config

--- Troll/config/application.rb
+++ Troll/config/application.rb
@@ -15,5 +15,14 @@ module Troll
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework :rspec, 
+      :fixtures => false,
+      :helper_specs => false,
+      :routing_specs => false,
+      :controller_specs => true,
+      :request_specs => false
+      g.fixture_replacement :factory_bot, :dir => "spec/factories"
+    end
   end
 end
