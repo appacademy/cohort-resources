4e96930923e5d02727bceb47b4e14e23b4784d6a
siascone <spencer.iascone@gmail.com>
2021-10-25 15:05:41 -0600

    Setup SF October 2021 Cohort Resources

--- assessment_tips/rails_2/rails_2_buggy_version/spec/controllers/application_controller_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe ApplicationController, type: :controller do
-  describe 'CSRF protection' do
-    it 'protects from forgery' do
-      expect(ApplicationController.new.forgery_protection_strategy).not_to be_nil
-    end
-  end
-end
--- study_guides/react_redux/pokedex_debugger/frontend/reducers/selectors.js
+++
@@ -1,9 +0,0 @@
-export const selectAllPokemon = state => Object.values(state.entities.pokemon);
-
-export const selectPokeItems = (state, poke) => {
-  return poke ? poke.item_ids.map(id => state.entities.items[id]) : [];
-};
-
-export const selectPokemonItem = (state, id) => {
-  return state.entities.items[id];
-};
