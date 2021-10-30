fd41c0fe7dde4f5601b8f176b5d94ec8b5551f7d
siascone <spencer.iascone@gmail.com>
2021-10-29 18:59:52 -0600

    add solutions to fa01p buggy

--- assessment_prep/foundations_01_prep/fa01p_buggy_solved/lib/practice_assessment_1.rb
+++ assessment_prep/foundations_01_prep/fa01p_buggy_solved/lib/practice_assessment_1.rb
@@ -6,7 +6,7 @@ def first_letter_vowel_count(sent)
     count = 0
 
     sent_arr.each do |word|
-        if vowels.include(word[0])
+        if vowels.include?(word[0].downcase)
             count += 1
         end
     end
@@ -20,10 +20,13 @@ def count_true(arr, prc)
     arr.each do |ele|
         if prc.call(ele)
             count += 1
+        end
     end
+
+    return count
 end
 
-def procformation(arr, &prc1, &prc2, &prc3)
+def procformation(arr, prc1, prc2, prc3)
     res = []
     arr.each do |ele|
         if prc1.call(ele)
@@ -36,13 +39,15 @@ def procformation(arr, &prc1, &prc2, &prc3)
 end
 
 def array_of_array_sum(arrays)
-    sum
+    sum = 0
 
     arrays.each do |arr|
         sum += arr.sum
     end
 
     sum
+
+    # arrays.flatten.sum
 end
 
 def selective_reverse(sent)
@@ -51,24 +56,34 @@ def selective_reverse(sent)
     new_sent_arr = []
 
     sent_arr.each do |word|
-        if !vowels.include?(word[0])
+        # check for front and back of each word looking for vowel
+        if !vowels.include?(word[0]) && !vowels.include?(word[-1])
             new_sent_arr << word.reverse
         else
             new_sent_arr << word
         end
     end
 
-    new_sent_arr
+    new_sent_arr.join(" ")
 end
 
-def hash_missing_keys(hash, additional_args)
+def hash_missing_keys(hash, *additional_args)
     res = []
 
     additional_args.each do |arg|
-        if hash[arg]
+        
+        if !hash.has_key?(arg)
             res << arg
         end
     end
 
     res
 end
+
+# hash = {
+#     1 => false,
+#     2 => true
+# }
+
+# p hash_missing_keys({"z"=> false, "f"=>-1, "c"=>32}, "z", "a")
+
