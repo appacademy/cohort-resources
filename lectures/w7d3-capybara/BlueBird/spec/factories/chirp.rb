439292e1c3b8f6e4aef8b0a0236f440fad4eca9c
Taylor Musolf <tcmusolf@gmail.com>
2022-04-05 23:06:39 -0700

    Remove spec files

--- lectures/w7d3-capybara/BlueBird/spec/factories/chirp.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do 
-  factory :chirp do 
-      body {"It's chirpalicious"} 
-      # We know though that chirps rely on a user/author. 
-      # FactoryBot makes this super easy for us with association syntax: 
-      association :author, factory: :user 
-  end 
-   
-end
