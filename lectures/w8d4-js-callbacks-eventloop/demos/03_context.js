918be20bdf61687c0992c4114922baefe71477b0
Taylor Musolf <tcmusolf@gmail.com>
2022-04-13 13:04:27 -0700

    Update to w8d4 notes and slides

--- lectures/w8d4-js-callbacks-eventloop/demos/03_context.js
+++ lectures/w8d4-js-callbacks-eventloop/demos/03_context.js
@@ -4,66 +4,72 @@
 
 
 
+class Cat {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function Cat(name) {
-    this.name = name;
-    debugger
+  constructor(name, owner) {
+      this.name = name;
+      this.owner = owner;
+      // debugger
   }
-  
-  Cat.prototype.meow = function () {
-    console.log(`${this.name} says 'meow'`);
-  };
-  
-  function Dog(name) {
-    this.name = name;
+
+  cuteStatement() {
+      return `${this.name} loves ${this.owner}. :3`;
   }
-  
-  Dog.prototype.bark = function () {
-    debugger
-    console.log(`${this.name} says 'woof'.`);
-  };
-  
-  Dog.prototype.sniff = function (otherThing) {
-    debugger
-    console.log(`${this.name} sniffs ${otherThing.name}`);
-  };
-  
-  
-  Cat.prototype.detachedMeow
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  // function timesDo(num, cb) {
-  //   for (let i = 0; i < num; i++) {
-  //     cb();
-  //   }
+
+  meow() {
+      // debugger
+      return `${this.name} says meow`;
+  }
+
+}
+
+// --------- loss of context, then preservation/binding of context ---------
+
+// First create a times function that will invoke a callback num times
+function times(num, callback) {
+  for (let i = 0; i < num; i++) {
+      console.log(callback())
+  }
+}
+
+// detachedMeow
+  garfield = new Cat('Garfield', 'Taylor')
+  detachedMeow = garfield.meow
+  // times(3, detachedMeow)
+
+// betterDetachedMeow
+  function betterDetachedMeow() {
+    return garfield.meow()
+  }
+
+// boundMeow
+  let boundMeow = detachedMeow.bind(garfield)
+// ---------------------------- context matters ----------------------------
+
+// Since the detachedMeow example is a bit isolated, let's see why context 
+// matters as we attempt to call instance methods inside other instance methods
+
+// Create a Cat.prototype.sayHi is going to call meow 3 times.
+
+Cat.prototype.sayHi = function() {
+  // const that = this
+
+  // return function() {
+  //     times(3, that.meow.bind(that))
   // }
-  
-  // Cat.prototype.makeNamePrinter() {
-  //   // Return a function that, when invoked, will print the cat's name
-  // }
+
+  return () => {
+      times(3, ()=>this.meow())
+  }
+}
+
+class Dog {
+  constructor(name, owner) {
+      this.name = name;
+      this.owner = owner;
+  }
+
+  bark(action) {
+      return `${this.name} says woof! and ${action}`
+  }
+}
--- lectures/w8d4-js-callbacks-eventloop/demos/05_readline.js
+++ lectures/w8d4-js-callbacks-eventloop/demos/05_readline.js
@@ -1,22 +1,37 @@
-// readline and require libraries are provided by node environment NOT by browser
+// readline and require libraries are provided by node environment NOT by the 
+// browser
 
+// Note: we'll only use this library today so we can practice asynchronous 
+// functions before using JS with the internet. It's a little finicky and 
+// you have to run in node, not chrome. 
+// - Run `node <filename>` instead of opening up node and `require`ing the file.
+// - Doing so in the browser will cause readline to be buggy and duplicate letters.
+
+// Syntax for setting up reader: don't need to memorize
 const readline = require("readline");
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-// .question(prompt, callback)
 
+// readline gives us an asynchronous question method that takes a prompt and 
+// a callback:  .question(prompt, callback)
 
 // What is wrong with the following code?
 
-const response = rl.question('What do you think of JavaScript? ', answer => answer);
-console.log(`Thank you for your valuable feedback: ${response}`);
+// const response = rl.question('What do you think of JavaScript? ', answer => answer);
+// console.log(`Thank you for your valuable feedback: ${response}`);
 
 const truth = rl.question('What do you really think of JavaScript? ', answer => answer);
 console.log(`You said: ${truth}. Thank you for your honesty.`);
 rl.close();
 
-
-
 // How do we fix it?
+
+// rl.question("What do you think of JavaScript? ", response => {
+//     console.log(`Thank you for your valuable feedback: ${response}`);
+//     rl.question("What do you really think of JavaScript? ", truth => {
+//         console.log(`You said ${truth}. Thank you for your honesty.`)
+//         rl.close()
+//     })
+// })
