9298e29a2a99518fbc61a771a85867eefcfc2047
Darren Eid <deid@appacademy.io>
2022-04-20 08:48:47 -0700

    add w9d3 slides and skeleton

---
+++ lectures/w9d3-jquery-ajax/giphy-demo/frontend/giphy_lite.js
@@ -0,0 +1,78 @@
+const GifApiUtil = require('./gif_api_util');
+
+// Install event listeners
+const setEventListeners = () => {
+  $('#new-gif-form').on('submit', e => {
+    e.preventDefault();
+    // Fetch a new GIF
+
+  });
+
+  $('#save-gif-form').on('submit', e => {
+    e.preventDefault();
+    // Save GIF
+
+  });
+
+  $('#old-gif-form').on('submit', e => {
+    e.preventDefault();
+    // Fetch saved GIF
+
+  });
+
+  $(".clear").on("click", () => {
+    // Clear out GIF display
+
+  });
+};
+
+$(setEventListeners);
+// setEventListeners will run after the dom is fully loaded
+// it adds all of our event listeners after we're sure the elements are on the page
+
+
+// ------------- GIF event handlers - fetchNew, save, fetchSaved, and clear ---------------
+
+const fetchNewGif = () => {
+  // Initiate AJAX call to GIPHY API, take response and put on the DOM
+
+};
+
+const saveGif = e => {
+  const $input = $('#save-gif-title');
+  const title = $input.val();
+  $input.val('');
+
+  // Initiate AJAX request to Rails backend, give a message if successful
+  // give an error if request was unsuccessful
+
+};
+
+const fetchSavedGif = () => {
+  const $input = $('#old-gif-query');
+  const title = $input.val();
+  $input.val('');
+
+  // Initate AJAX request to Rails backend, add GIF to the DOM if successful
+  // show errors if request was unsuccessful
+
+};
+
+const clearGif = () => {
+  const $gifDisplay = $(".gif-display");
+  $gifDisplay.empty();
+  return $gifDisplay;
+};
+
+
+// ------------- Other helper functions ---------------
+
+const appendGif = newGifURL => {
+  // Add gif to the display and keep track of its url
+  const $gifDisplay = $('.gif-display');
+  $gifDisplay.empty();
+
+  const $newImage = $(`<img class="gif" src="${newGifURL}">`);
+  $gifDisplay.data("url", newGifURL);
+  $gifDisplay.append($newImage);
+};
