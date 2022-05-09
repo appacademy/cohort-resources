1e384a0e86f5f841bfeb2eac461cd892777f254e
Darren Eid <darren.eid@gmail.com>
2022-05-08 21:50:11 -0700

    create initial react router materials

---
+++ lectures/w12d1-react-router/demo/app/helpers/api/api_helper.rb
@@ -0,0 +1,2 @@
+module Api::ApiHelper
+end
---
+++ lectures/w12d1-react-router/demo/frontend/components/app.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import TodoIndexContainer from './todo_index/todo_index_container';
+import TodoFormContainer from './todo_index/todo_form_container';
+import TodoShowContainer from './todo_index/todo_show_container';
+import Error from './error';
+import Header from './header';
+import Home from './home';
+
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+
+const App = () => (
+  <div className="app">
+    <Header />
+    <Home />
+    <TodoFormContainer />
+    <TodoIndexContainer />
+  </div>
+);
+
+export default App;
