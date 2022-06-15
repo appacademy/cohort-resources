b93734f1ae725805ea27396554a3af14f8b58e77
Mike Madsen <mikewmadsen@gmail.com>
2022-06-15 12:30:54 -0700

    Finish w9d3 demo

--- lectures/w09d3-jquery-ajax/giphy-demo-skeleton/frontend/giphy_lite.js
+++ lectures/w09d3-jquery-ajax/giphy-demo-skeleton/frontend/giphy_lite.js
@@ -5,24 +5,24 @@ const setEventListeners = () => {
   $('#new-gif-form').on('submit', e => {
     e.preventDefault();
     // Fetch a new GIF
-
+    fetchNewGif();
   });
 
   $('#save-gif-form').on('submit', e => {
     e.preventDefault();
     // Save GIF
-
+    saveGif();
   });
 
   $('#old-gif-form').on('submit', e => {
     e.preventDefault();
     // Fetch saved GIF
-
+    fetchSavedGif();
   });
 
   $(".clear").on("click", () => {
     // Clear out GIF display
-
+    clearGif()
   });
 };
 
@@ -35,7 +35,11 @@ $(setEventListeners);
 
 const fetchNewGif = () => {
   // Initiate AJAX call to GIPHY API, take response and put on the DOM
-
+  GifApiUtil.newGifAJAX()
+  .then(res => {
+    console.log(res);
+    appendGif(res.data.images.fixed_height_downsampled.url);
+  })
 };
 
 const saveGif = e => {
@@ -43,9 +47,32 @@ const saveGif = e => {
   const title = $input.val();
   $input.val('');
 
+  const url = $('.gif-display').data('url') // we set this inside appendGif function
+
+  const gifArg = {
+    title: title,
+    url: url
+  }
+
+  const $messages = $('.messages');
   // Initiate AJAX request to Rails backend, give a message if successful
   // give an error if request was unsuccessful
 
+  const success = (res) => {
+    $messages.text('you did it')
+    console.log("saved gif, good job")
+    setTimeout(() => $messages.empty(), 5000);
+  }
+  
+  const failure = (err) => {
+    $messages.text(err.responseJSON[0])
+    console.log("did not save, bad job >:(")
+    setTimeout(() => $messages.empty(), 5000);
+
+  }
+  
+  GifApiUtil.saveGifAJAX(gifArg)
+  .then(success, failure);
 };
 
 const fetchSavedGif = () => {
@@ -55,7 +82,12 @@ const fetchSavedGif = () => {
 
   // Initate AJAX request to Rails backend, add GIF to the DOM if successful
   // show errors if request was unsuccessful
-
+  GifApiUtil.fetchSavedGifAJAX(title)
+  .then( res => {
+    appendGif(res.url);
+  }, errors => {
+    console.log('gif not found :(');
+  })
 };
 
 const clearGif = () => {
