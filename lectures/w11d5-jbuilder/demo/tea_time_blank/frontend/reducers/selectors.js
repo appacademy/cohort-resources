bea544ec618d5774788b28f9ba5571302fdbc98d
Mike Madsen <mikewmadsen@gmail.com>
2022-07-01 08:43:12 -0700

    add demo setup for jbuilder

---
+++ lectures/w11d5-jbuilder/demo/tea_time_blank/frontend/reducers/selectors.js
@@ -0,0 +1,6 @@
+export const selectTransactionsByTea = (state, teaId) => {
+    if (state.teas[teaId].transactionIds === undefined) return []
+
+    return state.teas[teaId].transactionIds
+        .map(transactionId => state.transactions[transactionId]);
+};
