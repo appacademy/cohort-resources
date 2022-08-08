2ddfa6c4276360497a67534e4b4cf8dc4ca5cdf0
Paulo <paulobocanegra@gmail.com>
2022-08-07 21:34:29 -0700

    Create 08-08-2022 cohort branch

--- study_guides/react_redux/pokedex_debugger/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'static_pages#root'
-
-  namespace :api, defaults: { format: :json } do
-    resources :pokemon, only: [:index, :show, :create, :destroy]
-  end
-end
