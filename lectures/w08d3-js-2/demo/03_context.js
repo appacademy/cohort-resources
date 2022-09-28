49a3d6667c0b8d4c7d31350c13631d1400e5ef7e
Mike Madsen <mikewmadsen@gmail.com>
2022-09-28 09:07:54 -0700

    add w8d3 slides and demo skele

---
+++ lectures/w08d3-js-2/demo/03_context.js
@@ -0,0 +1,83 @@
+// Ways to call a function:
+//   ???
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function Cat(name) {
+  this.name = name;
+}
+
+Cat.prototype.meow = function () {
+  console.log(`${this.name} says 'meow'`);
+};
+
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype.bark = function () {
+  debugger
+  console.log(`${this.name} says 'woof'.`);
+};
+
+Dog.prototype.sniff = function (otherThing) {
+  debugger
+  console.log(`${this.name} sniffs ${otherThing.name}`);
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function timesDo(num, cb) {
+//   for (let i = 0; i < num; i++) {
+//     cb();
+//   }
+// }
+
+// Cat.prototype.makeNamePrinter() {
+//   // Return a function that, when invoked, will print the cat's name
+// }
