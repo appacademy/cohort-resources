e2e9be6878bce4fecc4b91295e42336445262107
paulobocanegra <paulobocanegra@gmail.com>
2022-10-04 12:19:44 -0700

    Add Vanilla JS lecture

--- lectures/w09d2-vanilla-js/vanilla-dom-skeleton/src/index.js
+++ lectures/w09d2-vanilla-js/vanilla-dom-skeleton/src/index.js
@@ -1,5 +1,14 @@
-//grab the dom elements we need (groceries, grocery-form, recipe-list, recipes)
 
+
+//grab the dom elements we need (groceries, grocery-form, recipe-list, recipes)
+let groceries = document.querySelector(".groceries") //UL in the middle
+let recipeList = document.querySelector(".recipe-list") // UL at the bottom
+let groceryForm = document.querySelector(".grocery-form") // FORM
+let recipes = document.querySelector(".recipes") // H3 on top
+
+// const exmapleArray = ["val1", "val2"]
+// localStorage.setItem("keyName", valueDesire) how to set localStorage key
+// localStorage.setItem("storageArray", exmapleArray)
 
 //create variables to hold localStorage things
 const lsItems = JSON.parse(localStorage.getItem('items')) || [];
@@ -7,24 +16,59 @@ const lsRecipes = JSON.parse(localStorage.getItem('recipes')) || [];
 
 //---------------PHASE 1 DOM MANIPULATION DEMO-----------------//
 //create event handler that adds to our grocery list
+
 const addItem = (e) => {
- 
+    e.preventDefault()
+    let input = document.querySelector("input[name='add-grocery']")
+    let value = input.value
+
+    const item = {value: value} // item = {value: "potato"}
+    lsItems.push(item)
+    localStorage.setItem("item", JSON.stringify(lsItems))
+
+    updateList();
+    groceryForm.reset()
+}   
+
+//create event handler to cross out list items on click
+const markAsDone = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+    let ele = e.target
+    console.log("HIT")
+    ele.classList.toggle('done')
 }
 
 //create action to render grocery list items
 const updateList = () => {
 
     // method 1: using string html element
+    // groceries.innerHTML = lsItems.map(item => {
+    //     return `<li>${item.value}</li>`
+    // }) 
 
-   
     // method 2: using document.createElement
+    if(!groceries.hasChildNodes()){
+        lsItems.map(item => {
+            let li = document.createElement("li")
+            li.innerHTML = item.value
+            groceries.appendChild(li)
+        })
+    } else {
+        let li = document.createElement("li")
+        li.innerHTML = lsItems[lsItems.length - 1].value
+        groceries.appendChild(li)
+    }
+
 
 }
 
-//create event handler to cross out list items on click
-const markAsDone = (e) => {
+// document.addEventListener("DOMContentLoaded", updateList)
 
-}
+
+
+
+
 
 //---------------PHASE III FOR WINDOW, LOCATION, HISTORY DEMO-----------------//
 
@@ -56,13 +100,29 @@ const updateWeeklyRecipe = () => {
 
 //---------------------------------------------------------------------//
 
+updateList()
 //add event listener to on submit for form to process add item
+groceryForm.addEventListener("submit", addItem)
+
+groceries.addEventListener("click", markAsDone)
 
 //add event listener to cross out a list item
+// let groc = document.querySelectorAll(".groceries li")
+// groc.forEach(child => {
+//     child.addEventListener("click", markAsDone)
+// })
+
+// QUERYSELECTORALL SUCKS
+
+
+
+
+
 
 //add event listener to to add recipe
 
 //call our methods to populate DOM
+
 
 
 //--------------------PHASE II EVENT BUBBLIING DEMO---------------------------------//
@@ -71,17 +131,15 @@ const updateWeeklyRecipe = () => {
 // groc.forEach((child) => {
 //     child.addEventListener("click", markAsDone);
 // })
-// const boo = (e) => {
-//     alert("Boo from the groceries ul! You didn't expect this, did you?!")
-// }
+const boo = (e) => {
+    alert("Boo from the groceries ul! You didn't expect this, did you?!")
+}
 
-// groceries.addEventListener("click", boo); //parent
+
+let parent = document.querySelector(".grocery-container")
+parent.addEventListener("click", boo); //parent
 
 //---------------------------------------------------------------------//
 
 
-//Script for LinkedIn endorsements
-// let skills = document.getElementsByClassName('pv-skill-entity__featured-endorse-button-shared');
-// for (let i = 0; i < skills.length; ++i) {
-//     skills[i].click();
-// }
+// localStorage.clear()
