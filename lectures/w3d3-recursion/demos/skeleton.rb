f5837cf2a4e6e3201a1e0ebf91ef51dcb4691b5e
tcmusolf@gmail.com <tcmusolf@gmail.com>
2022-10-18 22:36:49 -0700

    Add w3d3 slides and demo skeleton

---
+++ lectures/w3d3-recursion/demos/skeleton.rb
@@ -0,0 +1,87 @@
+require 'byebug'
+
+def iter_sum(n)
+
+end
+
+# p iter_sum(3) # => 6
+# p iter_sum(7) # => 28
+# p iter_sum(100) # => 5050
+
+
+def rec_sum(n)
+
+end
+
+# p "-------- RECURSION --------"
+
+# p rec_sum(3) # => 6
+# p rec_sum(7) # => 28
+# p rec_sum(100) # => 5050
+# p rec_sum(10000) # too many recursive calls breaks ruby's call stack / based on memory storage limit
+
+
+
+
+def rec_reverse(str) 
+ 
+end
+
+# p rec_reverse("cat") #=> 'tac'
+
+def iter_reverse(str)
+ 
+end  
+
+# p rec_reverse("cat") #=> 'tac'
+
+
+
+
+
+# Fibonacci examples:
+
+  #0,1,1,2,3,5,8,13,21,34
+
+# Return the nth Fibonacci number
+def fibs(n)
+  
+
+end 
+
+p fibs(1) #=> 0
+p fibs(2) #=> 1
+p fibs(3) #=> 1
+p fibs(4) #=> 2
+p fibs(5) #=> 3
+p fibs(6) #=> 5
+
+
+
+# Find all the Fibonacci numbers up to and including the nth
+def all_fibs(n) 
+
+
+end
+
+# p all_fibs(0)
+# p all_fibs(1)
+# p all_fibs(2)
+# p all_fibs(3)
+# p all_fibs(4)
+# p all_fibs(5)
+# p all_fibs(6)
+
+# now with memoization!
+def fast_all_fibs(n)
+
+
+end
+
+# p fast_all_fibs(0)
+# p fast_all_fibs(1)
+# p fast_all_fibs(2) 
+# p fast_all_fibs(3) 
+# p fast_all_fibs(4) 
+# p fast_all_fibs(5) 
+# p fast_all_fibs(6) 
