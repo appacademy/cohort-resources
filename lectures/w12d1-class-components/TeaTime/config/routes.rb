7b7cf465c10d6232b0585dd6ea8e73aa085da0d0
Mike Madsen <mikewmadsen@gmail.com>
2022-10-24 11:03:48 -0700

    Add w12d1 class lecture

---
+++ lectures/w12d1-class-components/TeaTime/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  namespace :api, defaults: {format: :json} do
+    resources :teas, only: [:index, :create, :show]
+  end
+  
+end
