fc03bf14f7e63621c6fefe7c71f4899a2160f83b
Taylor Musolf <tcmusolf@gmail.com>
2022-12-11 17:16:17 -0800

    w11d1 lecture

---
+++ lectures/w11d1-react-hooks/hooks-demo/src/App.js
@@ -0,0 +1,13 @@
+import NumbersDisplay from "./numbersDisplay";
+
+
+function App() {
+  return (
+    <>
+      <h1>Hello from App</h1>
+      <NumbersDisplay />
+    </>
+  );
+}
+
+export default App;
