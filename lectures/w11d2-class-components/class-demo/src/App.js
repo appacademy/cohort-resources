87ff50153bb71fd9c82cbae4402a20dbaf060520
Darren Eid <darren.eid@gmail.com>
2022-12-13 11:00:54 -0800

    add demo part two

--- lectures/w11d2-class-components/class-demo/src/App.js
+++ lectures/w11d2-class-components/class-demo/src/App.js
@@ -1,27 +1,56 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Time from "./components/Time";
 
-function App() {
-  console.log('rendering');
-  const [showTime, setShowTime] = useState(true);
+// function App() {
+//   console.log('rendering');
+//   const [showTime, setShowTime] = useState(true);
 
-  const clickHandler = e => {
-    e.preventDefault();
-    // preferred syntax whenever new value depends on old value
-    // pass in a callback that has the old value has a parameter
-    setShowTime(oldShowTime => (!oldShowTime));
-    // setShowTime(!showTime);
-  };
+//   const clickHandler = e => {
+//     e.preventDefault();
+//     // preferred syntax whenever new value depends on old value
+//     // pass in a callback that has the old value has a parameter
+//     setShowTime(oldShowTime => (!oldShowTime));
+//     // setShowTime(!showTime);
+//   };
   
-  return (
-    <>
-      <h1>Hello from App</h1>
-      <button onClick={clickHandler}>Toggle Time</button>
-      {showTime && (
-        <Time />
-      )}
-    </>
-  );
+//   return (
+//     <>
+//       <h1>Hello from App</h1>
+//       <button onClick={clickHandler}>Toggle Time</button>
+//       {showTime && (
+//         <Time />
+//       )}
+//     </>
+//   );
+// }
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showTime: true
+    };
+
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+
+  clickHandler(e) {
+    e.preventDefault();
+    this.setState(oldState => ({ showTime: !oldState.showTime }))
+  }
+
+  render() {
+    return(
+      <>
+        <h1>Hello from App</h1>
+        <button onClick={this.clickHandler}>Toggle Time</button>
+        {this.state.showTime && (
+          <Time />
+        )}
+     </>
+    )
+  }
 }
 
 export default App;
