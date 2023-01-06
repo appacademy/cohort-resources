b7c288315091c0df69957d930dba2d0069b7fd8c
Mike Madsen <mikewmadsen@gmail.com>
2023-01-06 12:15:13 -0800

    Add w5d5 active record dogebird changes

--- lectures/DogeBird/app/models/chirp.rb
+++ lectures/DogeBird/app/models/chirp.rb
@@ -34,5 +34,88 @@ class Chirp < ApplicationRecord
         through: :likes,
         source: :liker,
         dependent: :destroy
+
+
+    # chirp queries
+
+    # Find all of Chris's chirps
+    def self.find_chris_chirps
+        # Chirp.joins(:user).where("users.username = 'chrispy'")
+        Chirp.joins(:user).where(users: {username: 'chrispy'})
+    end
+
+    # Find all chirps liked by people whose favorite coin is Dogecoin
+    def self.find_doge_liked_chirps
+        # Chirp.joins(:user) # finds chirp authors
+        # Chirp.joins(:likes) # finds liker_ids of people who liked the chirps
+        Chirp.joins(:likers).where("users.favorite_coin = 'Dogecoin'")
+    end
+
+    # Find all chirps liked by people whose favorite coin is Dogecoin with no repeats
+    def self.find_doge_liked_chirps_no_repeats
+        Chirp.joins(:likers).where("users.favorite_coin = 'Dogecoin'").distinct
+    end
+
+    # Find all chirps with no likes
+    def self.no_likes
+        # Chirp.left_outer_joins(:likes).where(likes: {id: nil})
+        Chirp.left_outer_joins(:likes).where("likes.id IS NULL")
+    end
+
+    # Find how many likes each chirp has.
+    def self.num_likes_per_chirp
+        Chirp.select("count(*) as num_likes").joins(:likes).group(:chirp_id)
+    end
+
+    # Find chirps with at least 3 likes
+    def self.at_least_3_likes
+        Chirp.joins(:likes).group(:id).having("COUNT(*) >= 3")
+    end
+
+
+    
+     # Includes #
+
+    def self.see_chirp_authors_n_plus_one
+        # the "+1"
+        chirps = Chirp.all
+
+        # the "N"
+        chirps.each do |chirp|
+            puts chirp.user.username
+        end
+
+    end
+
+    def self.see_chirps_optimized
+        # pre-fetches data
+        chirps = Chirp.includes(:user).all
+
+        chirps.each do |chirp| 
+        # uses pre-fetched data 
+            puts chirp.user.username
+        end
+    end
+
+    # Joins #
+
+    def self.see_chirp_num_likes_n_plus_one
+        chirps = Chirp.all
+
+        chirps.each do |chirp|
+            puts chirp.likes.length
+        end
+    end
+
+    def self.see_chirp_num_likes_optimized
+        chirps_with_likes = Chirp
+            .select("chirps.*, COUNT(*) AS num_likes")
+            .joins(:likes)
+            .group(:id)
+    
+        chirps_with_likes.each do |chirp|
+            puts chirp.num_likes
+        end
+    end
+        
 end
-# chirp = Chirp.new(body: "ETH is king", author_id:)
--- lectures/DogeBird/app/models/user.rb
+++ lectures/DogeBird/app/models/user.rb
@@ -29,4 +29,38 @@ class User < ApplicationRecord
         through: :likes,
         source: :chirp,
         dependent: :destroy
+
+    # active record querys
+
+    # Find all users between the ages of 10 and 20 inclusive. Show their username and age
+    def self.find_young_users
+        # User.where("age >= 10 AND age <= 20").select("username, age") # more sql looking
+        User.where(age: 10..20).select(:username, :age) # more ruby looking
+    end
+
+    # Find all users not younger than the age of 11. use where not
+    def self.find_not_younger
+        # User.where.not("age < 11")
+        User.where.not("age < ?", 11)
+    end
+
+    # Find all of our users favorite coins
+    def self.find_favorite_coins
+        User.select("favorite_coin").group("favorite_coin")
+        # User.select("favorite_coin").distinct
+    end
+
+    # Find all users with the given favorite coins and order by ascending name
+    def self.find_certain_coins
+        fav_coins = ["Bitcoin", "Ethereum"]
+        # User.where("favorite_coin IN (?)", fav_coins).order("username ASC")
+        User.order(username: :asc).where(favorite_coin: fav_coins)
+    end
+
+    # Find all chirps for a particular user -> chrispy
+    def self.find_chris_chirps
+        User.find_by(username: "chrispy").chirps
+    end
+
+        
 end
