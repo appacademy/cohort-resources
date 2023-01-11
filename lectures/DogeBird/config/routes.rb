06a24b31a90e44d22c26561625ed1a4995f1ee7e
Paulo <paulobocanegra@gmail.com>
2023-01-11 13:56:11 -0800

    Add controller to DogeBird

--- lectures/DogeBird/config/routes.rb
+++ lectures/DogeBird/config/routes.rb
@@ -3,4 +3,23 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # get '/chirps', to: 'chirps#index'
+  # get '/chirps/:id', to: 'chirps#show'
+  # post '/chirps', to: 'chirps#create'
+  # patch '/chrips/:id', to: 'chirps#update'
+  # delete '/chirps/:id', to: 'chirps#destroy'
+
+  # get '/chrips/search', to: 'chirps#search'
+
+  resources :chirps, only:[:index, :show, :create, :update, :destroy]
+
+  #nested routes
+  # resources :users do
+  #   resources :chirps, only: [:update, :create]
+  # end
+
+  # resources :chirps, only: [:index, :show]
+
+
 end
