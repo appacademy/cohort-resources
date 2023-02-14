b0b0dfd01561c38887fd2a75a4e88caee0aac09d
Paulo <paulobocanegra@gmail.com>
2023-02-14 11:51:01 -0800

    Add demo to react class components

--- lectures/w11d2-class-components/class-demo/src/App.js
+++ lectures/w11d2-class-components/class-demo/src/App.js
@@ -1,12 +1,48 @@
 import React, { useState } from "react";
 import Time from "./components/Time";
 
-function App() {
-  return (
-    <>
-      <h1>Hello from App!</h1>
-    </>
-  );
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      showTime: true,
+      fruit: "banana"
+    }
+    this.clickHandler = this.clickHandler.bind(this)
+  }
+
+  clickHandler(e){
+    e.preventDefault();
+    this.setState(oldState => ({showTime: !oldState.showTime}))
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Hello from App!</h1>
+        <button onClick={this.clickHandler}>Toggle Time</button>
+        {/* <Time banana={"banana"} /> */}
+        {this.state.showTime && (
+          <Time fruit={this.state.fruit}/>
+        ) }
+      </>
+    );
+  }
 }
 
+
+// function App() {
+
+  // const [showTime, setShowtime] = useState(true)
+
+//   return (
+//     <>
+//       <h1>Hello from App!</h1>
+//       <Time banana={"banana"} />
+//     </>
+//   );
+// }
+
 export default App;
+
