fb001dd3efc53d13802ee81a36eb00c1e840c58c
Paulo <paulobocanegra@gmail.com>
2023-04-10 13:50:35 -0700

    Add demos & lecture to ruby into 03 27 2023

--- lectures/w03d1-ruby-review/demos/blocks_and_procs.rb
+++ lectures/w03d1-ruby-review/demos/blocks_and_procs.rb
@@ -2,7 +2,7 @@ require 'byebug'
 
 class Array
   def my_each_block(&prc)
-    debugger
+    # debugger
     i = 0
     while i < self.length
       prc.call(self[i])
@@ -12,13 +12,21 @@ class Array
   end
 
   def my_each_proc(prc)
-
+    my_each_block(&prc)
+    # debugger
+    # i = 0
+    # while i < self.length
+    #   prc.call(self[i])
+    #   i +=1
+    # end
+    # self
   end
 end
 
 arr = [1,2,3]
+arr.each {|ele| ele += 1}
 arr.my_each_block { |el| p el }
 # arr.my_each_proc { |el| p el } # throws Argument Error - needs proc
 
 prc = Proc.new { |el| p el }
-# arr.my_each_proc(prc)
+arr.my_each_proc(prc)
--- lectures/w03d1-ruby-review/demos/instance_and_class_methods.rb
+++ lectures/w03d1-ruby-review/demos/instance_and_class_methods.rb
@@ -1,17 +1,53 @@
 class Fish
     
+    @@all_fish = []
+
+    def self.all
+        @@all_fish
+    end
+
+    def self.random_state_of_being
+        [true, false].sample
+    end
+
+    def self.random_name
+        ["Thomas", "Nitty", "Rocco", "Misha"].sample
+    end
+
+    def self.make_more
+        self.new(self.random_name, self.random_state_of_being)
+    end
+
+    def self.make_all
+        99.times do 
+            name = ""
+            5.times do
+                name += ('a'..'z').to_a.sample
+            end
+            Fish.new(name, self.random_state_of_being)
+        end
+        self.all
+    end
+
+    # attr_reader :name
+    # attr_writer :name
+    attr_accessor :name
+
     def initialize(name, lost)
         @name = name
         @lost = lost
+        @@all_fish << self
     end
     
-    def name
-        @name
-    end
-
     def lost?
         @lost
     end
 
 end
+
+rant = "You could write really, really, really long lines in Ruby but that is bad code style should be avoided.
+ Do not have more than 80 characters on one line."
+
+
+
 
