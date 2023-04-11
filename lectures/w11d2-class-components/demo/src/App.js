a5d04883e0de8cae5e5ca62216a6f86de287452a
Taylor Musolf <tcmusolf@gmail.com>
2023-04-11 12:16:11 -0700

    demo for w11d2

--- lectures/w11d2-class-components/demo/src/App.js
+++ lectures/w11d2-class-components/demo/src/App.js
@@ -1,12 +1,58 @@
 import React, { useState } from "react";
 import Time from "./components/Time";
 
-function App() {
-  return (
-    <>
-      <h1>Hello from App!</h1>
-    </>
-  );
+class App extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      time: false
+    }
+    this.clickHandler = this.clickHandler.bind(this);
+
+  }
+
+  clickHandler(e){
+    e.preventDefault();
+    this.setState({time: !this.state.time})
+  }
+
+
+
+  render(){
+    const {time} = this.state;
+    return (
+      <>
+        <h1>Hello from App!</h1>
+        <button onClick={this.clickHandler}>Toggle Time</button>
+        {time &&
+          <Time />
+        }
+      </>
+    );
+  }
 }
 
 export default App;
+
+
+
+// function App() {
+//   const [showTime, setShowTime] = useState(false);
+
+//   const clickHandler = (e)=>{
+//     e.preventDefault();
+//     setShowTime((oldShowTime)=> !oldShowTime)
+//   }
+//   return (
+//     <>
+//       <h1>Hello from App!</h1>
+//       <button onClick={clickHandler}>Toggle Time</button>
+//       {showTime &&
+//         <Time />
+//       }
+//     </>
+//   );
+// }
+
+// export default App;
--- lectures/w11d2-class-components/demo/src/components/Time.jsx
+++ lectures/w11d2-class-components/demo/src/components/Time.jsx
@@ -1,1 +1,106 @@
 // TODO: write a clock component
+
+import React, {useState, useEffect} from 'react';
+
+class Time extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      timeData: {}
+    }
+  }
+
+  componentDidMount(){
+
+    const getTime = async ()=>{
+  
+      try{
+        let res = await fetch('http://worldtimeapi.org/api/ip');
+        if(res.ok){
+          let data = await res.json();
+          this.setState({timeData: data});
+        }
+      } catch{
+        console.error('no dice!')
+      }
+  
+    }
+  
+    this.timeRequest = setInterval(getTime, 1000);
+  
+  }
+  
+  componentWillUnmount(){
+    console.log('interval cleared')
+    clearInterval(this.timeRequest)
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    // console.log('prevProps', prevProps)
+    // console.log('prevState', prevState)
+    if(prevState.timeData !== this.state.timeData){
+      console.log(this.state.timeData)
+    }
+
+  }
+
+
+  // useEffect(()=>{
+  //   console.log(timeData);
+  // }, [timeData])
+
+  render(){
+    return(
+      <p>{this.state.timeData.datetime ? `It is currently: ${this.state.timeData.datetime}` : 'Loading...'}</p>
+    )
+  }
+
+}
+
+
+export default Time;
+
+
+// function Time(props){
+
+//   const [timeData, setTimeData] = useState({});
+
+
+//   useEffect(()=>{
+//     const getTime = async ()=>{
+
+//       try{
+//         let res = await fetch('http://worldtimeapi.org/api/ip');
+//         if(res.ok){
+//           let data = await res.json();
+//           setTimeData(data);
+//         }
+//       } catch{
+//         console.error('no dice!')
+//       }
+
+//     }
+
+//     const timeRequest = setInterval(getTime, 1000);
+
+//     return(()=>{
+//       console.log('interval cleared')
+//       clearInterval(timeRequest)
+//     })
+
+//   }, [])
+
+//   useEffect(()=>{
+//     console.log(timeData);
+//   }, [timeData])
+
+
+//   return(
+//     <p>{timeData.datetime ? `It is currently: ${timeData.datetime}` : 'Loading...'}</p>
+//   )
+
+// }
+
+
+// export default Time;
