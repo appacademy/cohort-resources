bccef4150b5d5bf5f724545056afeaced5485d2a
Taylor Musolf <tcmusolf@gmail.com>
2023-04-13 14:12:28 -0700

    demo notes for w11d4

--- lectures/w11d4-react-redux/tea-shop/src/App.js
+++ lectures/w11d4-react-redux/tea-shop/src/App.js
@@ -1,10 +1,12 @@
 import TeaIndex from "./components/TeaIndex";
+import TeaForm from "./components/TeaForm";
 
 function App() {
   return (
     <>
       <h1>Hello from App</h1>
       <TeaIndex />
+      <TeaForm />
     </>
   );
 }
