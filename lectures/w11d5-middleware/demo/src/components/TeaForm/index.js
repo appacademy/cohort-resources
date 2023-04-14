f69634e277dcf0e8adecf3f597c1ac31defe797e
Darren Eid <darren.eid@gmail.com>
2023-04-14 08:38:42 -0700

    add middleware slides

---
+++ lectures/w11d5-middleware/demo/src/components/TeaForm/index.js
@@ -0,0 +1,48 @@
+import {useState} from 'react';
+import { useDispatch } from 'react-redux';
+import {receiveTea} from '../../store/teaReducer';
+import './index.css'
+
+const TeaForm = (props) => {
+    const dispatch = useDispatch();
+
+    const [flavor, setFlavor] = useState('');
+    const [price, setPrice] = useState('');
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        const id = Math.floor(Math.random()* 1000)
+        //temporary id assignment.  Db will assign in the future
+
+        const tea = {
+            id,
+            flavor,
+            price
+        }
+        dispatch(receiveTea(tea));
+       
+        //clear our form
+        setFlavor('');
+        setPrice('');
+    }
+
+    return(
+        <div className='tea-form'>
+            <h2>I'm the Tea Form</h2>
+            <form onSubmit={handleSubmit}>
+                <label> Flavor:
+                    <input value={flavor} onChange={(e)=> setFlavor(e.target.value)}/>
+                </label>
+                <label> Price:
+                    <input value={price} onChange={(e)=> setPrice(e.target.value)}/>
+                </label>
+                <input type='submit' value='Add Tea'/>
+            </form>
+        </div>
+    )
+
+
+}
+
+export default TeaForm;
---
+++ lectures/w11d5-middleware/demo/src/store/teaReducer.js
@@ -0,0 +1,52 @@
+const RECEIVE_TEA = 'RECEIVE_TEA';
+const RECEIVE_TEAS = 'RECEIVE_TEAS';
+const REMOVE_TEA = 'REMOVE_TEA'
+
+
+
+//ACTION CREATORS
+
+export const receiveTea = tea => {
+    return {
+        type: RECEIVE_TEA,
+        tea: tea
+    }
+}
+
+export const receiveTeas = teas => {
+    return {
+        type: RECEIVE_TEAS,
+        teas //teas: teas
+    }
+}
+
+export const removeTea = teaId => (
+    {
+        type: REMOVE_TEA,
+        teaId
+    }
+)
+
+//REDUCER
+
+const teaReducer = (state = {}, action) => {
+    Object.freeze(state);
+    const nextState = {...state};
+
+    switch(action.type){
+        case RECEIVE_TEA:
+            nextState[action.tea.id] = action.tea;
+            return nextState;
+        case RECEIVE_TEAS:
+            return {...nextState, ...action.teas}    
+        case REMOVE_TEA:
+            delete nextState[action.teaId]
+            return nextState;
+        default:
+            return state;
+    }
+
+    
+    
+}
+export default teaReducer;
