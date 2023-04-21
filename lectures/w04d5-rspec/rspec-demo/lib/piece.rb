565840c6e25787df885cc2049d7d47867babfdae
Mike Madsen <mikewmadsen@gmail.com>
2023-04-21 14:26:38 -0700

    finish rspec demo and slides

--- lectures/w04d5-rspec/rspec-demo/lib/piece.rb
+++ lectures/w04d5-rspec/rspec-demo/lib/piece.rb
@@ -2,10 +2,28 @@ class Piece
 
     attr_reader :color, :pos
 
-    def initialize(color, pos)
+    def initialize(color, board, pos)
         raise "Invalid color (must be black or white)" unless [:black, :white].include?(color)
+        raise "Invalid pos" unless board.valid_pos?(pos)
         @color = color
+        @board = board
         @pos = pos
     end
+
+    def pos=(new_pos)
+        @pos = new_pos
+    end
+
+    def to_s
+        ' 👑 '
+    end
+
+    def empty?
+        false
+    end
     
+end
+
+class Board
+
 end
--- lectures/w04d5-rspec/rspec-demo/spec/piece_spec.rb
+++ lectures/w04d5-rspec/rspec-demo/spec/piece_spec.rb
@@ -1,10 +1,13 @@
 require "piece"
 
 describe Piece do
+    # let(:board) {Board.new} # integration test
+    let(:board) {double("Board", valid_pos?: true)} # creates fake Board instance, where valid_pos? returns true
+    subject(:piece) { Piece.new(:white, board, [0,0]) }
+
     describe "#initialize" do
         context "with valid arguments" do
             it "instantiates a piece correctly" do
-                piece = Piece.new(:white, [0,0])
                 expect(piece.color).to be(:white)
                 expect(piece.pos).to eq([0,0])
             end
@@ -12,29 +15,31 @@ describe Piece do
 
         context "with invalid arguments" do
             it "raises an error when provided the wrong color" do
-                expect{ Piece.new(:blue, [0,0]) }.to raise_error("Invalid color (must be black or white)")
+                expect{ Piece.new(:blue, board, [0,0]) }.to raise_error("Invalid color (must be black or white)")
+            end
+
+            it "raises an error while provided an invalid condition" do
+                allow(board).to receive(:valid_pos?).and_return(false) # change valid_pos? for one test
+                expect{ Piece.new(:white, board, [-1, 99])}.to raise_error("Invalid pos")
             end
         end
     end
 
     describe "#pos=" do
+        before(:each) {piece.pos = [1,1]} # before(:all) errors out
         it "correctly reassign the position of a piece" do
-            piece = Piece.new(:white, [0,0])
-            piece.pos = [1,1]
             expect(piece.pos).to eq([1,1])
         end
     end
 
     describe "#to_s" do
         it "returns a string" do
-            piece = Piece.new(:white, [0,0])
             expect(piece.to_s).to eq(' 👑 ')
         end
     end
 
     describe "#empty?" do
         it "returns false" do
-            piece = Piece.new(:white, [0,0])
             expect(piece.empty?).to be false
         end
     end
