9303ec20981c20f2c0fe434be6c0bc7d6cc736d9
Mike Madsen <mikewmadsen@gmail.com>
2023-04-28 12:15:57 -0700

    add demo changes AR

--- AIBird/app/models/chirp.rb
+++ AIBird/app/models/chirp.rb
@@ -4,10 +4,17 @@
 #
 #  id         :bigint           not null, primary key
 #  body       :text             not null
-#  user_id    :bigint           not null
+#  user_id    :bigint           not null <-- foreign key represents the user who wrote it
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
+
+# FROM chirps
+# JOIN users
+# ON chirps.user_id = users.id
+
+## Chirp.joins(:author)
+
 class Chirp < ApplicationRecord
     validates :body, :user_id, presence: true
     validate :body_too_long
@@ -36,6 +43,83 @@ class Chirp < ApplicationRecord
         dependent: :destroy
 
 
+    # Find all chirps for a particular user - ai_mike
+    def self.find_user_chirps
+        Chirp.joins(:author).where(users: {username: 'ai_mike'})
+        # Chirp.joins(:author).where("users.username = 'ai_mike'")
+    end
 
+    # Find all chirps liked by people affiliated with humans
+    def self.find_human_liked_chirps
+        Chirp.joins(:likers).where(users: {affiliation: 'human'})
+        # Chirp.joins(:likers).where("users.affiliation = 'human'")
+    end
+
+    # Find all chirps with no likes
+    def self.chirps_no_likes
+        Chirp.left_joins(:likes).where(likes: {id: nil})
+    end
+
+    # Find how many likes each chirp has. display id body and num likes
+    def self.chirps_num_likes
+        Chirp.select(:id, :body, "COUNT(likes.chirp_id) as num_likes").joins(:likes).group(:id)
+        # Chirp.select(:id, :body, "COUNT(likes.chirp_id) as num_likes").left_joins(:likes).group(:id)
+        # Chirp.select(:id, :body, "COUNT(likes.chirp_id) as num_likes").left_joins(:likes).group(:id).order(num_likes: :desc)
+    end
+
+    # Find chirps with at least 2 likes. use pluck to get the body!
+    def self.pluck_two_or_more_liked_chirps
+        # Chirp.chirps_num_likes.having("COUNT(*) >= 2").pluck(:body)
+        # Chirp.joins(:likes).group(:id).having("COUNT(chirps.id) >= 2").pluck(:id, :body)
+        Chirp.joins(:likes).group(:id).having("COUNT(chirps.id) >= 2").pluck()
+    end
+
+     # Includes #
+
+     def self.see_chirp_authors_n_plus_one
+        # the "+1"
+        chirps = Chirp.all
+
+        # the "N"
+        chirps.each do |chirp|
+            puts chirp.author.username
+        end
+        nil
+
+    end
+
+    def self.see_chirps_optimized
+        # pre-fetches data
+        chirps = Chirp.includes(:author).all
+
+        chirps.each do |chirp| 
+        # uses pre-fetched data 
+            puts chirp.author.username
+        end
+        nil
+    end
+
+    # Joins #
+
+    def self.see_chirp_num_likes_n_plus_one
+        chirps = Chirp.all
+
+        chirps.each do |chirp|
+            puts chirp.likes.length
+        end
+        nil
+    end
+
+    def self.see_chirp_num_likes_optimized
+        chirps_with_likes = Chirp
+            .select("chirps.*, COUNT(*) AS num_likes")
+            .joins(:likes)
+            .group(:id)
+    
+        chirps_with_likes.each do |chirp|
+            puts chirp.num_likes
+        end
+        nil
+    end
 
 end
--- AIBird/app/models/user.rb
+++ AIBird/app/models/user.rb
@@ -25,6 +25,36 @@ class User < ApplicationRecord
         class_name: :Like,
         dependent: :destroy
 
+    # Find all users with an evil score between 1 and 5 inclusive, show username and affiliation
+
+    def self.nice_users
+        User.select(:username, :affiliation).where("evil_score BETWEEN 1 AND 5")
+        # User.select(:username, :affiliation).where(evil_score: 1..5)
+    end
+
+    # Find all users that don't have an evil_score less than 9. Use where not
+    def self.evil_users
+        User.select(:username).where.not("evil_score < 9")
+    end
+
+    # Find all affiliation of our users. use group
+    def self.all_affiliations
+        User.select(:affiliation).group(:affiliation)
+    end
+
+    # Find all users who has as affiliation in this list and order by username ascending
+    def self.find_affiliates
+        affiliations = ['AI', 'human']
+        User.where("affiliation IN (?)", affiliations).order("username")
+        # User.where("affiliation IN (['AI', 'human'])").order("username")
+        # User.where(affiliation: affiliations).order(username: :ASC)
+    end
+
+    # Find all chirps for a particular user, use an association
+    def self.find_aimike_chirps
+        # User.where("username = 'ai_mike'").first.chirps
+        User.find_by(username: 'ai_mike').chirps
+    end
     
 
 end
