54f59321d95cdcb8f2c1c3afa10a621a8f73a9c7
Mike Madsen <mikewmadsen@gmail.com>
2023-06-08 19:28:04 -0700

    add pre lecture middleware slides

---
+++ lectures/w11d5-middleware/demo/src/App.js
@@ -0,0 +1,14 @@
+import AddTeaForm from "./components/AddTeaForm.js";
+import TeaIndex from "./components/TeaIndex.js";
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to the Magic Tea Shop</h1>
+      <TeaIndex />
+      <AddTeaForm />
+    </>
+  );
+}
+
+export default App;
---
+++ lectures/w11d5-middleware/demo/src/store/teaReducer.js
@@ -0,0 +1,41 @@
+const RECEIVE_TEA = 'RECEIVE_TEA';
+const RECEIVE_TEAS = 'RECEIVE_TEAS';
+const REMOVE_TEA = 'REMOVE_TEA';
+
+/* ----ACTION CREATORS---- */
+export const receiveTea = tea => ({
+  type: RECEIVE_TEA,
+  tea
+});
+
+export const receiveTeas = teas => ({
+  type: RECEIVE_TEAS,
+  teas
+});
+
+export const removeTea = teaId => ({
+  type: REMOVE_TEA,
+  teaId
+});
+
+/* ----REDUCER---- */
+const teaReducer = (state = {}, action) => {
+  Object.freeze(state);
+  const nextState = { ...state };
+  console.log(action);
+
+  switch (action.type) {
+    case RECEIVE_TEA:
+      nextState[action.tea.id] = action.tea;
+      return nextState;
+    case RECEIVE_TEAS:
+      return { ...nextState, ...action.teas };
+    case REMOVE_TEA:
+      delete nextState[action.teaId];
+      return nextState;
+    default:
+      return nextState;
+  };
+};
+
+export default teaReducer;
