bcc31af49e5de223cb1542734b395b2321c92eac
Mike Madsen <mikewmadsen@gmail.com>
2023-06-13 09:38:36 -0700

    add changes before break 1

--- lectures/w12d2-fullstack-auth/teatime/app/controllers/application_controller.rb
+++ lectures/w12d2-fullstack-auth/teatime/app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+
+  protect_from_forgery with: :exception # csrf
+  before_action :snake_case_params, :attach_authenticity_token
+
+  private
+  def snake_case_params
+    params.deep_transform_keys!(&:underscore) # snake case any camel case params from our frontend
+  end
+
+  def attach_authenticity_token
+    headers['X-CSRF-Token'] = masked_authenticity_token(session)
+  end
+  
 end
--- lectures/w12d2-fullstack-auth/teatime/config/environment.rb
+++ lectures/w12d2-fullstack-auth/teatime/config/environment.rb
@@ -6,3 +6,4 @@ Rails.application.initialize!
 
 
 Jbuilder.key_format camelize: :lower
+Jbuilder.deep_format_keys true
