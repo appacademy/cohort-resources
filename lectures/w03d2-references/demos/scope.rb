19de1b777eae04326fc6d5c65dff04d6102d368f
paulobocanegra <paulobocanegra@gmail.com>
2023-08-01 11:23:40 -0700

    Add slides

---
+++ lectures/w03d2-references/demos/scope.rb
@@ -0,0 +1,59 @@
+#example 1
+# x = 2
+
+# 3.times do
+#   x *= 2
+# end
+
+# puts x
+
+
+#example 2
+# 3.times do 
+#     x = 2
+# end
+
+# puts x
+
+#example 3
+# x = 10
+
+# def some_method
+#     puts x
+# end
+
+# some_method
+
+#example 4
+
+# def include_x(num)
+#     num += 1
+# end 
+
+# a = 1
+
+# include_x(a)
+
+# puts a
+
+#example 5
+
+# def add_square(arr, num)
+#     arr << (num ** 2)
+# end
+
+# squares= [1,4,9]
+# add_square(squares, 4)
+
+# p squares
+
+# class Cat
+#     def initialize(name)
+#         @name = name
+#     end
+
+#     def mehtod_name
+#         puts "hello"
+#     end
+# end
+
