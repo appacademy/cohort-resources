3c61998a5c71708578e9bca280ecd45be2683f5c
Darren Eid <darren.eid@gmail.com>
2023-08-02 09:14:55 -0700

    add initial redux slides

---
+++ lectures/w11d03-redux/sample_state.js
@@ -0,0 +1,60 @@
+sampleState = {
+    posts: {
+        1: {
+            id: 1,
+            authorId: 1,
+            body: "......",
+            comments: [1, 2]
+        },
+        2: {
+            id: 2,
+            authorId: 2,
+            body: "......",
+            comments: [3, 4, 5]
+        }
+    },
+    comments: {
+        1: {
+            id: 1,
+            authorId: 2,
+            comment: ".....",
+        },
+        2: {
+            id: 2,
+            authorId: 3,
+            comment: ".....",
+        },
+        3: {
+            id: 3,
+            authorId: 3,
+            comment: ".....",
+        },
+        4: {
+            id: 4,
+            authorId: 4,
+            comment: ".....",
+        },
+        5: {
+            id: 5,
+            authorId: 3,
+            comment: ".....",
+        }
+    },
+    users: {
+        1: {
+            id: 1,
+            username: "user1",
+            name: "User 1",
+        },
+        2: {
+            id: 2,
+            username: "user2",
+            name: "User 2",
+        },
+        3: {
+            id: 3,
+            username: "user3",
+            name: "User 3",
+        }
+    }
+}
