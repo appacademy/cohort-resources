6809344644a3f4660b5f1d26feb5b6b3365f0c8a
Mike Madsen <mikewmadsen@gmail.com>
2023-08-22 15:03:21 -0700

    add meta demo

--- lectures/w06d2-metaprogramming/demo.rb
+++ lectures/w06d2-metaprogramming/demo.rb
@@ -1,25 +1,62 @@
 require 'byebug'
 
 class Pet
+
+  def self.add_pet
+    # class variables
+    @@total_pets ||= 0
+    @@total_pets += 1
+    # class instance variables
+    # @total_pets ||= 0
+    # @total_pets += 1
+  end
+
+  def self.total_pets
+    @@total_pets
+  end
+    
   def initialize(name)
     @name = name
   end
 end
 
 class Dog < Pet
-  puts self
+  puts self # Dog class
 
   def self.doggie_things
-    puts self
+    puts self # Dog class
+  end
+
+  def self.create_tricks(*trick_names)
+    p self # Dog class
+    trick_names.each do |trick_name|
+        define_method(trick_name) do |thing = 'chain saws', n = 1| # create a method with that name on the Dog class
+            p self # instance
+            n.times do
+                puts "#{trick_name}ing with #{thing}!!!"
+            end
+        end
+    end
   end
 
   def who_am_i?
-    puts self
+    puts self # instance of Dog
   end
 
   def initialize(name)
     super(name)
     @secret = "I'm a goat in disguise"
+    self.class.add_pet # Dog.add_pet
+  end
+  
+  def do_this_and_then_that(this, that)
+    self.send(this) # calls method stored in this variable
+    self.send(that) # calls method in that variable
+    # cant do self.this ## would look for literally a method named 'this'
+  end
+
+  def do_some_method_with_args(some_method, *args)
+    self.send(some_method, *args)
   end
 
   def sleep
@@ -52,3 +89,12 @@ class Dog < Pet
     puts "My secret is: #{@secret}"
   end
 end
+
+
+# def my_attr_reader(*method_names)
+    # iterate over method names
+    # define methods with those names
+    # return instance variables with those names
+# end
+
+    # if setter, sets instance variables to values passed in
