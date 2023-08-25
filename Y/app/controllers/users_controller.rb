b56593f33e80b9ec53f10da98edb253ab2843cc6
Mike Madsen <mikewmadsen@gmail.com>
2023-08-25 12:31:15 -0700

    add views to Y

--- Y/app/controllers/users_controller.rb
+++ Y/app/controllers/users_controller.rb
@@ -1,38 +1,49 @@
 class UsersController < ApplicationController
 
+  def new
+    render :new
+  end
+
   def index
-    users = User.all
-    render json: users
+    @users = User.all
+    render :index
   end
 
   def create
-    user = User.new(user_params)
+    @user = User.new(user_params)
     
-    if user.save
-      redirect_to user_url(user)
+    if @user.save
+      redirect_to user_url(@user)
     else
-      render json: user.errors.full_messages, status: 422
+      render json: @user.errors.full_messages, status: 422
     end
   end
 
   def show
-    user = User.find_by(id: params[:id])
-    render json: user
+    # local variable user can only be read in this method definition
+    # Instance variable @user can be read in views / html erb templates
+    @user = User.find_by(id: params[:id])
+    render :show
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+    render :edit
   end
 
   def update
-    user = User.find_by(id: params[:id])
+    @user = User.find_by(id: params[:id])
 
-    if user.update(user_params)
-      redirect_to user_url(user)
+    if @user.update(user_params)
+      redirect_to user_url(@user)
     else
-      render json: user.errors.full_messages, status: 422
+      render json: @user.errors.full_messages, status: 422
     end
   end
 
   def destroy
-    user = User.find_by(id: params[:id])
-    user.destroy
+    @user = User.find_by(id: params[:id])
+    @user.destroy
     redirect_to users_url
   end
 
