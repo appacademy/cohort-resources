2183c097c8dcef14b0659df9b9a956c008804aa1
paulobocanegra <paulobocanegra@gmail.com>
2023-09-08 12:23:17 -0700

    Add demo to prototypes

--- lectures/w08d5-prototypes/demos/myBind.js
+++ lectures/w08d5-prototypes/demos/myBind.js
@@ -1,30 +1,70 @@
-class Cat {
-    constructor(name) {
-        this.name = name;
+class Dog{
+    constructor(name){
+        this.name = name
     }
 
-    meow() {
-        console.log(`${this.name} says meow!`);
+    bark(){
+        // this
+        console.log(`${this.name} says woof`)
+    }
+
+    greet(d1, d2, d3){
+        console.log(`${this.name} says woof to ${d1.name}, ${d2.name}, and ${d3.name}`)
     }
 }
 
-function Cat(name){
-    this.name = name
+Function.prototype.myBind = function(ctx){
+    let that = this; // 'this' is the receiver of my bind 
+    return function(){
+        that.apply(ctx)
+    }
 }
 
-Cat.prototype.meow = function(){
-    console.log(`${thisname} says meow!`)
+Function.prototype.myBind2 = function(ctx){
+    let that = this; // 'this' is the receiver of my bind 
+    const bindArgs = Array.from(arguments).slice(1);
+    return function(){
+        const callArgs = Array.from(arguments)
+        that.apply(ctx, bindArgs.concat(callArgs))
+    }
+}
+
+Function.prototype.myBind3 = function(ctx, ...bindArgs){
+    let that = this; // 'this' is the receiver of my bind 
+    return function(...callArgs){
+        that.apply(ctx, bindArgs.concat(callArgs))
+    }
+}
+
+Function.prototype.myBind3 = function(ctx, ...bindArgs){
+    let that = this; // 'this' is the receiver of my bind 
+    return function(...callArgs){
+        that.call(ctx, ...bindArgs, ...callArgs)
+    }
 }
 
 
 
-const drogo = new Cat("Drogo");
-drogo.meow();
-// setTimeout(drogo.meow, 1000);
-// setTimeout(drogo.meow.myBind(drogo), 1000);
-
-// setTimeout(drogo.meow.myBind(drogo, arg1,arg2)(arg3,arg4), 1000);
-// setTimeout(drogo.meow.bind(drogo), 1000);
 
 
+let dog = new Dog("Remy");
+let dog2 = new Dog("Fido1");
+let dog3 = new Dog("Fido2");
+let dog4 = new Dog("Fido3");
 
+// dog.bark()
+// setTimeout(dog.bark, 1000) // we lose context because callback functions are invoked func style (ctx: window/global)
+// setTimeout(dog.bark.bind(dog), 1000)
+// setTimeout(dog.bark.myBind(dog), 1000)
+
+
+// setTimeout(dog.bark, 1000)
+// setTimeout(dog.greet.myBind3(dog, dog2,dog3,dog4), 1000)
+
+dog.greet.myBind3(dog)(dog2,dog3,dog4)
+dog.greet.myBind3(dog, dog2)(dog3,dog4)
+dog.greet.myBind3(dog, dog2, dog3, dog4)()
+
+// setTimeout(dog.greet.myBind2(dog, dog2, dog3, dog4), 1000)
+
+// setTimeout(dog.greet.myBind2(dog)(dog2,dog3,dog4), 1000) //This does not work because we must pass an uninvoked function to setTimeout, once we run it with bind time arguments, the function returns undefined and we are basically trying to run undefined(dog2,dog3,dog4)
--- lectures/w08d5-prototypes/demos/prototypalInheritance.js
+++ lectures/w08d5-prototypes/demos/prototypalInheritance.js
@@ -1,70 +1,86 @@
-function Animal (name) {
-    this.name = name;
-};
+// function Animal (name) {
+//     this.name = name;
+// };
 
-Animal.prototype.eat = function () {
-    console.log(this.name + ' is eating.');
-};
+// Animal.prototype.eat = function () {
+//     console.log(this.name + ' is eating.');
+// };
 
-function Cat(name) {
-    this.name = name;
-}
+// Animal.prototype.setName = function(name){
+//     this.name = name
+// }
 
-// // Make Cat inherit from Animal here.
-// Cat.prototype = Animal.prototype // NOPE, because all Animals could meow
+// function Cat(name) {
+//     Animal.call(this, name)
+// }
+
+
+
+
+// // // Make Cat inherit from Animal here.
+// // Cat.prototype = Animal.prototype // NOPE, because all Animals could meow
 // // Cat.prototype = new Animal() // NOPE, because it would be expensive if Animal's constructor had a lot of logic
 
-function Surrogate(){}
-Surrogate.prototype = Animal.prototype;
-Cat.prototype = new Surrogate()
-Cat.prototype.constructor = Cat;
-
-
 // function Surrogate(){}
-// Surrogate.prototype = Parent.prototype;
-// Child.prototype = new Surrogate()
-// Child.prototype.constructor = Child;
-
-// Cat.prototype = Object.create(Animal.prototype) // {walk: f(), eat: f()}
+// Surrogate.prototype = Animal.prototype;
+// Cat.prototype = new Surrogate()
 // Cat.prototype.constructor = Cat;
 
 
-Cat.prototype.meow = function () {
-    console.log('Meow!');
-};
+// Cat.prototype.meow = function () {
+//     console.log('Meow!');
+// };
 
 
-const garfield = new Cat('Garfield');
-// garfield.eat();
+
+// // function Surrogate(){}
+// // Surrogate.prototype = Parent.prototype;
+// // Child.prototype = new Surrogate()
+// // Child.prototype.constructor = Child;
+
+
+
+
+// // Cat.prototype = Object.create(Animal.prototype) // {walk: f(), eat: f()}
+// // Cat.prototype.constructor = Cat;
+
+
+
+// const garfield = new Cat('Garfield');
+// // garfield.eat();
+// // garfield.meow();
+
+// const noCat = new Animal('noCat');
+// // noCat.meow();
+
 // garfield.meow();
-
-const noCat = new Animal('noCat');
-noCat.meow();
 
 
 // ES6
 
-// class Animal {
-//     constructor(name){
-//         this.name = name
-//     }
+class Animal {
+    constructor(name){
+        this.name = name
+    }
 
-//     eat(food){
-//         console.log(`${this.name} is eating the ${food}`)
-//     }
-// }
+    eat(food){
+        console.log(`${this.name} is eating the ${food}`)
+    }
+}
 
-// class Cat extends Animal {
-//     constructor(name, furColor){
-//         // super(name) // Animal.call(this)
-//         this.name = name
-//         this.furColor = furColor
-//     }
+class Cat extends Animal {
+    constructor(name, furColor){
+        super(name)                     // Animal.call(this)
+        // this.name = name'
+        // Animal.call(this, name) //Nope: Must call super constructor in derived class before accessing 'this' or returning from derived constructor
+        this.furColor = furColor
+    }
 
-//     meow(){
-//         console.log(`${this.name} says meow!`)
-//     }
-// }
+    meow(){
+        console.log(`${this.name} says meow!`)
+    }
+}
 
-// drogo = new Cat("drogo", "tux");
-// drogo.meow();
+drogo = new Cat("drogo");
+drogo.meow();
+console.log(drogo)
