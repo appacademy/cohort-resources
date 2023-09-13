7b4cd9f256ec05333b12935f65dd6ba2950274fa
paulobocanegra <paulobocanegra@gmail.com>
2023-09-12 18:17:17 -0700

    Add slides and demo skeleton to vanilla js lecture

---
+++ lectures/w09d3-vanilla-js/demo/index.js
@@ -0,0 +1,22 @@
+//grab the dom elements we need (groceries, grocery-form, recipe-list, recipes)
+
+
+
+
+//---------------PHASE 1 DOM MANIPULATION DEMO-----------------//
+//create event handler that adds to our grocery list
+
+
+//create event handler to cross out list items on click
+
+
+
+
+
+//---------------------------------------------------------------------//
+
+//add event listener to on submit for form to process add item
+
+//add event listener to cross out a list item
+
+//add event listener to to add recipe
