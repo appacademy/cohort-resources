e3b2e67487c52435e0e5edc68795a8e8c7a64633
Darren Eid <darren.eid@gmail.com>
2024-03-05 15:12:23 -0800

    add ajax slides and skeleton

---
+++ lectures/w09d3-ajax/frontend/src/index.js
@@ -0,0 +1,14 @@
+console.log('webpack is working');
+
+const giphyUrl = 'https://api.giphy.com/v1/gifs/random?api_key=3yvEbPRnIhHJhjKS9vBRQjQQDCHme7rh&rating=g&tag=???';
+
+
+// querySelectors
+
+// fetchNewGif
+
+// clearGif
+
+// renderNewGif
+
+// eventListeners
